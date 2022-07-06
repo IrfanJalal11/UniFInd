9eeeb20f6428d6cbebad6871616f11826f86dbe5
fajarh8 <fajar0heryan@gmail.com>
2022-07-07 00:14:11 +0700

    fix navbar

--- universitas/universitas.php
+++ universitas/universitas.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>UniFind: Home Page</title>
+        <title>UniFind: Universitas</title>
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <style>
@@ -61,58 +61,7 @@
           </div>
         </div>
       </nav>
-
-      <!-- sponsored-content -->
-      <div class="pt-3 pb-3">
-        <img src="assets/images/kuliah-bsi.jpg" alt="sponsored-content" style="width: 100%; height: 400px;">
-      </div>
-
-      <!-- Menu Cards -->
-      <div class="row p-3">
-        <div class="col-sm-2" style="width: 25%">
-          <div class="card">
-            <img src="assets/images/illustration.jpg" class="card-img-top" alt="Ilustrasi Universitas">
-            <div class="card-body">
-              <h5 class="card-title text-center fw-bold p-1">UNIVERSITAS</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ligula purus, bibendum in congue non, aliquet nec lacus. Cras at sodales mauris.</p>
-              <a href="universitas/universitas.php" class="btn btn-primary" style="margin-left: 55%">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2" style="width: 25%">
-          <div class="card">
-            <img src="assets/images/illustration.jpg" class="card-img-top" alt="Ilustrasi Unitry">
-            <div class="card-body">
-              <h5 class="card-title text-center fw-bold p-1">UNITRY</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ligula purus, bibendum in congue non, aliquet nec lacus. Cras at sodales mauris.</p>
-              <a href="unitry/unitry.php" class="btn btn-primary" style="margin-left: 55%">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2" style="width: 25%">
-          <div class="card">
-            <img src="assets/images/illustration.jpg" class="card-img-top" alt="Ilustrasi Beasiswa">
-            <div class="card-body">
-              <h5 class="card-title text-center p-1 fw-bold">BEASISWA</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ligula purus, bibendum in congue non, aliquet nec lacus. Cras at sodales mauris.</p>
-              <a href="beasiswa/beasiswa.php" class="btn btn-primary" style="margin-left: 55%">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-2" style="width: 25%">
-          <div class="card">
-            <img src="assets/images/illustration.jpg" class="card-img-top" alt="Ilustrasi Donasi">
-            <div class="card-body">
-              <h5 class="card-title text-center fw-bold p-1">DONASI</h5>
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ligula purus, bibendum in congue non, aliquet nec lacus. Cras at sodales mauris.</p>
-              <a href="donasi/donasi.php" class="btn btn-primary" style="margin-left: 55%">Selengkapnya</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <!-- Bootstrap JS -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     </body>
-</html>
-
+</html>  
